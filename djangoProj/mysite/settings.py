009c443715b1860d309383b03e81e11ece30320e
deepakbehera11 <deepakbehera3717@gmail.com>
2025-06-12 07:28:16 +0000

    update ALLOWED_HOST

--- djangoProj/mysite/settings.py
+++ djangoProj/mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-1l31t4^99j%i)=)j*mxgm$@thpycp%+xio8_fxz0!b0blk$@w=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
